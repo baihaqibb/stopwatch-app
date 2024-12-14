765d62018c0b72a1b9b7656ec03b89a71d292140
baihaqibb <138758831+baihaqibb@users.noreply.github.com>
2024-12-14 17:16:03 +0700

    publish to github

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import Stopwatch from "./Stopwatch"
+
+function App() {
+
+  return (
+    <>
+      <div className="stopwatch-app">
+        <h1 className="title">STOPWATCH APP</h1>
+        <Stopwatch/>
+      </div>
+    </>
+  )
+}
+
+export default App
